2c5d8ea3e0d0ced76dbb16f89fdea9c918d48c25
Sion Gang <siongang@gmail.com>
2025-04-22 22:44:13 -0400

    music carousel

--- personal_website/src/app/page.tsx
+++ personal_website/src/app/page.tsx
@@ -1,13 +1,12 @@
-'use client';
+"use client";
 
-import NavButton from '@/components/NavButton';
-import ProjectCarousel from '@/components/ProjectCarousel';
+import MusicCarousel from "@/components/MusicCarousel";
+import NavButton from "@/components/NavButton";
+import ProjectCarousel from "@/components/ProjectCarousel";
 
 export default function HomePage() {
   return (
-    <main
-      className="h-screen w-full overflow-x-hidden  snap-y snap-mandatory scroll-smooth bg-black text-white"
-    >
+    <main className="h-screen w-full overflow-x-hidden  snap-y snap-mandatory scroll-smooth bg-black text-white">
       {/* HERO SECTION */}
       <section className="h-screen flex flex-col justify-center items-center text-center px-6 snap-start">
         <h1 className="text-6xl md:text-7xl font-extrabold tracking-wide mb-12">
@@ -24,28 +23,34 @@ export default function HomePage() {
       {/* PROJECTS */}
       <section
         id="projects"
-        className="h-screen flex flex-col justify-center items-center snap-start bg-[#111]"
+        className="h-screen flex flex-col justify-center items-center snap-start bg-black "
       >
-        <div className="flex">  
+        <div className="flex">
           <h2 className="text-4xl font-bold">Projects</h2>
         </div>
-        <div className = "flex flex-row w-1/2">
-        <ProjectCarousel />
+        <div className="flex flex-row justify-center w-1/2">
+          <ProjectCarousel />
         </div>
       </section>
 
       {/* MUSIC */}
       <section
         id="music"
-        className="h-screen flex justify-center items-center snap-start bg-[#181818]"
+        className="h-screen flex flex-col justify-center items-center snap-start bg-black "
       >
-        <h2 className="text-4xl font-bold">Music</h2>
+        <div className="flex">
+          <h2 className="text-4xl font-bold">Music</h2>
+        </div>
+
+        <div className="flex flex-row justify-center w-1/2">
+          <MusicCarousel />
+        </div>
       </section>
 
       {/* EXPERIENCE */}
       <section
         id="experience"
-        className="h-screen flex justify-center items-center snap-start bg-[#222]"
+        className="h-screen flex justify-center items-center snap-start bg-black "
       >
         <h2 className="text-4xl font-bold">Experience</h2>
       </section>
@@ -53,7 +58,7 @@ export default function HomePage() {
       {/* CONTACT */}
       <section
         id="contact"
-        className="h-screen flex justify-center items-center snap-start bg-[#282828]"
+        className="h-screen flex justify-center items-center snap-start bg-black "
       >
         <h2 className="text-4xl font-bold">Contact</h2>
       </section>
---
+++ personal_website/src/components/MusicCarousel.tsx
@@ -0,0 +1,13 @@
+
+
+
+export default function MusicCarousel() {
+
+	return(
+		<div>
+			hi
+		</div>		
+
+
+	);    
+}
