d45df7427f1f517ae4ecb63e8c794fa482f55ad5
Sion Gang <siongang@gmail.com>
2025-04-26 13:44:57 -0400

    adjs

--- personal_website/src/app/page.tsx
+++ personal_website/src/app/page.tsx
@@ -7,9 +7,9 @@ import { Mail, Github, Linkedin, Instagram, Link } from "lucide-react";
 
 export default function HomePage() {
   return (
-    <div className="h-screen w-full flex flex-col bg-black text-white">
-  <main className="flex-1 overflow-y-auto snap-y snap-mandatory scroll-smooth ">
-  {/* HERO SECTION */}
+    <div className="h-screen w-full flex flex-col bg-black text-white overflow-x-hidden">
+      <main className="flex-1 overflow-y-auto snap-y snap-mandatory scroll-smooth ">
+        {/* HERO SECTION */}
         <section className="min-h-screen flex flex-col justify-center items-center text-center px-6 snap-start">
           <h1 className="text-6xl md:text-7xl font-extrabold tracking-wide mb-12">
             SION GANG
@@ -35,7 +35,7 @@ export default function HomePage() {
               Waterloo.`}
               <br></br>
               {`I'm also a problem solver 🤔, music creator 🎧, tech enthusiast ⚙️, and a former national-stage kicker 🥋`}
-              </p>
+            </p>
             <div className="flex items-center gap-6 mt-4">
               <a href="mailto:siongang@gmail.com" target="_blank">
                 <Mail className="w-6 h-6 hover:opacity-80 transition" />
@@ -72,7 +72,7 @@ export default function HomePage() {
         {/* PROJECTS */}
         <section
           id="projects"
-          className="h-screen flex flex-col justify-center items-center snap-start bg-black "
+          className="min-h-screen flex flex-col justify-center items-center snap-start bg-black "
         >
           <div className="flex">
             <h2 className="text-4xl font-bold">Projects</h2>
@@ -95,7 +95,7 @@ export default function HomePage() {
         {/* MUSIC */}
         <section
           id="music"
-          className="h-screen flex flex-col justify-center items-center snap-start bg-black "
+          className="min-h-screen flex flex-col justify-center items-center snap-start bg-black "
         >
           <div className="flex">
             <h2 className="text-4xl font-bold">Music</h2>
@@ -124,10 +124,13 @@ export default function HomePage() {
     */}
       </main>
 
-  {/* Fixed height footer, NOT fixed position anymore */}
-  <footer className="w-full bg-black text-gray-500 text-xs text-center py-2 border-t border-gray-700">
-    <p>© {new Date().getFullYear()} Sion Gang. Built with too much instant ramen and not enough sleep.</p>
-  </footer>
-      </div>
+      {/* Fixed height footer, NOT fixed position anymore */}
+      <footer className="w-full bg-black text-gray-500 text-xs text-center py-2 border-t border-gray-700">
+        <p>
+          © {new Date().getFullYear()} Sion Gang. Built with too much instant
+          ramen and not enough sleep.
+        </p>
+      </footer>
+    </div>
   );
 }
